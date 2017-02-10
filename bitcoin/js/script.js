b178e7a970f0253ab20ae8dfa7a8586ab1daa0c3
MarinaAbdou <marina.abdou1996@gmail.com>
2017-02-10 19:08:04 +0100

    Update script.js

--- bitcoin/js/script.js
+++ bitcoin/js/script.js
@@ -3,8 +3,8 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById("info").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.open("GET", "http://bitcoin.mubiz.com/info", true);
 xmlhttp.send();
