8418a558da5f877e9b3e60455600403e81e6360c
MarinaAbdou <marina.abdou1996@gmail.com>
2017-02-10 22:25:55 +0100

    Update script.js

--- bitcoin/js/script.js
+++ bitcoin/js/script.js
@@ -1,13 +1,27 @@
 function info() 
 {
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
         document.getElementById("info").innerHTML = jsonPretty;
     }
-};
-xmlhttp.open("GET", "http://bitcoin.mubiz.com/info", true);
-xmlhttp.send();
+    };
+    xmlhttp.open("GET", "http://bitcoin.mubiz.com/info", true);
+    xmlhttp.send();
+}
+
+function blockchaininfo()
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("blockchaininfo").innerHTML = jsonPretty;
+    }
+    };
+    xmlhttp.open("GET", "http://bitcoin.mubiz.com/blockchaininfo", true);
+    xmlhttp.send();
 }
